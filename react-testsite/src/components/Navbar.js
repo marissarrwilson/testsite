638fa6a1b83ebed996bf70ed2417d7c9d6bd27c3
Mars <wilson.marissarose@gmail.com>
2021-01-15 17:37:28 -0500

    Created button.js and .css in components folder

--- react-testsite/src/components/Navbar.js
+++ react-testsite/src/components/Navbar.js
@@ -1,42 +1,50 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-    return (
-        <>
-         <nav className='navbar'>
-             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    Marissa Wilson <i className='fab fa-typo3'/>
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
-                            Portfolio - Past Projects
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-             </div> 
-         </nav>
-        </>
-    );
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Marissa Wilson <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/projects"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Portfolio - Past Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
