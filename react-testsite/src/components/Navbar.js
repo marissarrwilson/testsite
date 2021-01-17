fbed80571f3f9cf73cc3d23e876ed17acefbfb70
Mars <wilson.marissarose@gmail.com>
2021-01-17 18:06:38 -0500

    Added images and videos folder, HeroSection.js and .css, pages folder with Home.js component

--- react-testsite/src/components/Navbar.js
+++ react-testsite/src/components/Navbar.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Marissa Wilson <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -28,7 +46,7 @@ function Navbar() {
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
-                Portfolio - Past Projects
+                Past Projects
               </Link>
             </li>
             <li className="nav-item">
@@ -40,7 +58,18 @@ function Navbar() {
                 Contact
               </Link>
             </li>
+            {/* <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign up
+              </Link>
+            </li> */}
           </ul>
+
+          {/* {button && <Button buttonStyle="btn--outline">SIGN UP</Button>} */}
         </div>
       </nav>
     </>
