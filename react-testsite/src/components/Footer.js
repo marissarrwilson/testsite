5e7af7e3546ec13675040b572679a5850af6c514
Mars <wilson.marissarose@gmail.com>
2021-01-18 16:06:55 -0500

    Added Footer to main page w/ <p>

---
+++ react-testsite/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className='footer-subscription'>
+                <p className='footer-subscription-text'>
+                    Join the adv newsletter to receive etc.
+                </p>
+            </section>
+        </div>
+    )
+}
+
+export default Footer
