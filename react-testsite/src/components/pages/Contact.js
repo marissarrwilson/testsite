99beaa29563cead42a6c49c6657cc6a1052bfb3d
Mars <wilson.marissarose@gmail.com>
2021-01-23 15:55:15 -0500

    Created ContactSection.js/.css and ProjectSection.js/.css

--- react-testsite/src/components/pages/Contact.js
+++ react-testsite/src/components/pages/Contact.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "../../App.css";
 // import HeroSection from '../HeroSection'
-import Footer from '../Footer';
+import Footer from "../Footer";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function Contact() {
-  return(
-      <>
-        <h1 className="contact">CONTACT</h1>;
-        <Footer />
-        
-      </>
+  return (
+    <>
+      <h1 className="contact">CONTACT</h1>;
+      <Footer />
+    </>
   );
-//   ) 
-  
 }
 
 export default Contact;
